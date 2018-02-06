a372b1e96c144dc78659437b6dbd96f3678252ce
alex-bajzat <alex.bajzat@gmail.com>
2018-02-06 20:10:33 +0200

    added base components

---
+++ src/main/java/com/bjz/cnninference/activations/Activation.java
@@ -0,0 +1,7 @@
+package com.bjz.cnninference.activations;
+
+/**
+ * Created on 2/6/2018.
+ */
+public interface Activation {
+}
---
+++ src/main/java/com/bjz/cnninference/layers/Layer.java
@@ -0,0 +1,7 @@
+package com.bjz.cnninference.layers;
+
+/**
+ * Created on 2/6/2018.
+ */
+public interface Layer {
+}
