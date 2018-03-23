e89b1827672ed5978b77e2aa097b93d08d8cbbbe
alex-bajzat <alex.bajzat@gmail.com>
2018-03-23 12:04:58 +0200

    Changed android incompatible foreach

--- src/main/java/com/bjz/cnninference/model/Model.java
+++ src/main/java/com/bjz/cnninference/model/Model.java
@@ -32,12 +32,12 @@ public class Model {
      * @return {@link PredictionResult} containing probabilites and raw results for each index
      */
     public PredictionResult predict(double[][][] input) {
-        for (ComplexLayer layer : complexLayers) {
-            input = layer.forward(input);
+        for (int i = 0; i < complexLayers.size(); i++) {
+            input = complexLayers.get(i).forward(input);
         }
         double[][] flatten = transitionLayer.apply(input);
-        for (SimpleLayer layer : simpleLayers) {
-            flatten = layer.forward(flatten);
+        for (int i = 0; i < simpleLayers.size(); i++) {
+            flatten = simpleLayers.get(i).forward(flatten);
         }
 
         double[] rawResult = flatten[0];
